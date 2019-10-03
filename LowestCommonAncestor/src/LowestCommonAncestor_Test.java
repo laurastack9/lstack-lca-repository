fa6eba0ad87807545443b4de6ce813d2d3aac72c
Laura <lstack@tcd.ie>
2019-10-03 14:06:12 +0100

    Created test class

---
+++ LowestCommonAncestor/src/LowestCommonAncestor_Test.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LowestCommonAncestor_Test {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
