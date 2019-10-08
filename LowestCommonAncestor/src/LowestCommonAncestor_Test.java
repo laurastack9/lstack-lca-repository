8fdd7f0f52ec150d8cfad9b8ce4c81e7175f51c6
Laura <lstack@tcd.ie>
2019-10-08 11:40:01 +0100

    Added some potential tests

--- LowestCommonAncestor/src/LowestCommonAncestor_Test.java
+++ LowestCommonAncestor/src/LowestCommonAncestor_Test.java
@@ -5,8 +5,26 @@ import org.junit.jupiter.api.Test;
 class LowestCommonAncestor_Test {
 
 	@Test
-	void test() {
+	public void test() {
 		fail("Not yet implemented");
 	}
-
+	
+	@Test
+	public void testLCA() {
+		
+		//add test
+	}
+	
+	@Test 
+	public void testContains() {
+		
+		//add test
+	}
+	
+	@Test 
+	public void testHeight() {
+		
+		//add test
+	}
+	
 }
