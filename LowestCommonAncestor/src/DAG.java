ac5f71f2e3f4e2a07b6e66f4aa233b7a03cbda8d
Laura <lstack@tcd.ie>
2019-10-12 11:45:25 +0100

    Started working on DAG class

--- LowestCommonAncestor/src/DAG.java
+++ LowestCommonAncestor/src/DAG.java
@@ -1,6 +1,15 @@
 
-public class DAG {
-	
-	//This class will be used to implement the graph
+public class DAG <Value>{
 
+	private class Node {
+
+		private Value val;					// Value stored in Node
+		private Node[] successors;			// Array of nodes' successors
+
+		public Node(Value val) {
+			this.val = val;
+		}
+		
+		private Node[] nodeList = new DAG.Node[0];
+	}
 }
