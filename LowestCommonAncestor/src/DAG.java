ae99da1b7d7be9f4a94544165a0d7a1fabebd7fc
Laura <lstack@tcd.ie>
2019-10-14 12:55:15 +0100

    Made a put method
    Need to write method to extend the arrays to accommodate any new nodes.

--- LowestCommonAncestor/src/DAG.java
+++ LowestCommonAncestor/src/DAG.java
@@ -9,14 +9,14 @@ public class DAG <Value>{
 		public Node(Value val) {
 			this.val = val;
 		}
-		
+
 		private Node[] nodeList = new DAG.Node[0];
-		
-		
+
+
 		public int size() { 
 			return nodeList.length;  //returns number of nodes the DAG
 		}
-		
+
 		public boolean isEmpty() {  //method to check if the DAG is empty
 			if (size()==0) {
 				return true;
@@ -25,17 +25,42 @@ public class DAG <Value>{
 				return false;
 			}
 		}
-		
+
 		// Converts value to a Node by taking a value and returning associated Node. 
-				public Node retrieveNodefromValue (Value v){
-					Node nodeToRetrieve = new Node(null); 				
-					for (int i=0; i<nodeList.length; i++){
-						if (nodeList[i].val == v){
-							nodeToRetrieve = nodeList[i];
-							break;			
-						}	
-					}
-					return nodeToRetrieve;
+		public Node retrieveNodefromValue (Value v){
+			Node nodeToRetrieve = new Node(null); 				
+			for (int i=0; i<nodeList.length; i++){
+				if (nodeList[i].val == v){
+					nodeToRetrieve = nodeList[i];
+					break;			
+				}	
+			}
+			return nodeToRetrieve;
+		}
+
+		public void put(Value v, Value fromVal, Value toVal) {	
+			Node n = new Node(v);
+			Node from = retrieveNodefromValue(fromVal);
+			Node to = retrieveNodefromValue(toVal);
+
+			if (fromVal != null) {
+				from.successors = extendArray(from.successors); //will create extendArray
+				from.successors[from.successors.length-1] = n;
+				if (from.val == null) {
+					// If 'from' Node is not already in the graph? To be updated
 				}
+			}
+
+			if (toVal != null) {
+				//where does this get connected to?
+			}
+		}
+
 	}
+
+
+
+
+
+}
 }
