250e6039755ec34703fd913da84d642f62637653
Laura <lstack@tcd.ie>
2019-10-15 19:51:43 +0100

    Added tests to LCA test file for put, delete, DAGcontains and DAGempty
    Need to resolve errors

--- LowestCommonAncestor/src/LowestCommonAncestor_Test.java
+++ LowestCommonAncestor/src/LowestCommonAncestor_Test.java
@@ -7,6 +7,13 @@ class LowestCommonAncestor_Test {
 
 	
 	@Test
+	public void testDAGLCA() {
+		//test to be written here for testing LCA for a DAG
+		//to be updated
+	}
+	
+	
+	@Test
 	public void testLCA() {
 		
 		LowestCommonAncestor<Integer, Integer> BST = new LowestCommonAncestor<Integer, Integer>();
@@ -29,9 +36,74 @@ class LowestCommonAncestor_Test {
 	}
 	
 	@Test
-	public void testDAGLCA() {
-		//test to be written here for testing LCA for a direct acyclic graph
-		//will be written at a later date
+	public void testPut() {
+		LowestCommonAncestor<Integer, Integer> LCA = new LowestCommonAncestor<Integer, Integer>();
+		LCA.put(1, null);
+		LCA.put(10, 1);
+		LCA.put(15,2);
+		LCA.put(15, 15);	 
+
+		assertEquals("Putting nodes", "(()10(()15()))", LCA.printKeysInOrder());
+	}
+	
+	@Test
+	public void testDelete() {
+		LowestCommonAncestor<Integer, Integer> LCA = new LowestCommonAncestor<Integer, Integer>();
+		LCA.delete(1);
+		assertEquals("Deleting from empty tree", "()", LCA.printKeysInOrder());
+
+		LCA.put(7, 7);   //        _7_
+		LCA.put(8, 8);   //      /     \
+		LCA.put(3, 3);   //    _3_      8
+		LCA.put(1, 1);   //  /     \
+		LCA.put(2, 2);   // 1       6
+		LCA.put(6, 6);   //  \     /
+		LCA.put(4, 4);   //   2   4
+		LCA.put(5, 5);   //        \
+		//                           5
+
+		assertEquals("Checking order of constructed tree",
+				"(((()1(()2()))3((()4(()5()))6()))7(()8()))", LCA.printKeysInOrder());
+
+		LCA.delete(9);
+		assertEquals("Deleting non-existent key",
+				"(((()1(()2()))3((()4(()5()))6()))7(()8()))", LCA.printKeysInOrder());
+
+		LCA.delete(8);
+		assertEquals("Deleting leaf", "(((()1(()2()))3((()4(()5()))6()))7())", LCA.printKeysInOrder());
+
+		LCA.delete(6);
+		assertEquals("Deleting node with single child",
+				"(((()1(()2()))3(()4(()5())))7())", LCA.printKeysInOrder());
+
+		LCA.delete(3);
+		assertEquals("Deleting node with two children",
+				"(((()1())2(()4(()5())))7())", LCA.printKeysInOrder());
+	}
+	
+	@Test
+	public void testDAGContains(){
+		DAG<Integer> dag = new DAG <Integer>();
+
+		assertEquals("Checking contains() on empty dag.", false, dag.contains(10));
+
+		dag.put(7, null, null); 		//        (7)
+		dag.put(8, 7, null);   			//   	  (7) -> (8) -> 10
+		for(int i = 0; i<1; i++) {
+			System.out.println(dag.toString());
+		}
+		System.out.println(dag.size());
+	//	assertEquals("Checking DAG contains() a particular Value.", true, dag.contains(8));
+	}
+
+	@Test
+	public void testDAGEmpty() {
+		DAG<Integer> dag = new DAG<Integer>();
+		assertEquals("Checking isEmpty() on empty dag.", true, dag.isEmpty());
+		dag.put(1, null, null);
+		//add a value 1 with null to and from pointers (its the first value)
+		System.out.println(dag.size());
+		assertEquals("Checking isEmpty() on empty dag.", false, dag.isEmpty());
 	}
 	
 	@Test 
