f341eb54d3bfb3fd391c715f7bd6396dccc7ec88
Laura <lstack@tcd.ie>
2019-10-16 16:25:10 +0100

    Implementing BFS/reverse methods
    Used methods from CS2010. Using reverse method to traverse the graph
    backwards by reversing the links, and finding the first vertice where
    the graphs cross.

--- LowestCommonAncestor/src/DAG.java
+++ LowestCommonAncestor/src/DAG.java
@@ -1,123 +1,159 @@
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.List;
 
-public class DAG <Value>{
+public class DAG {
 
-	private class Node {
+	private int V;           // number of vertices in this digraph
+	private int E;                 // number of edges in this digraph
+	private ArrayList<Integer>[] adj;    // adj[v] = adjacency list for vertex v
+	private int[] indegree;        // indegree[v] = indegree of vertex v
+	private boolean marked[];		//Boolean List to track visited vertices
+	private boolean hasCycle;		//True if cycle in graph
+	private boolean stack[];		//Order that vertices were visited
+	private int[] edgeTo;      // edgeTo[v] = last edge on shortest s->v path
+	private int[] distTo;      // distTo[v] = length of shortest s->v path
 
-		private Value val;					// Value stored in Node
-		private Node[] successors;			// Array of nodes' successors
 
-		public Node(Value val) {
-			this.val = val;
+	public DAG(int V)
+	{
+		if (V < 0) throw new IllegalArgumentException("Number of vertices in a Digraph must be nonnegative");
+		this.V = V;
+		this.E = 0;
+		indegree = new int[V];
+		marked = new boolean[V];
+		stack = new boolean[V];
+		adj = (ArrayList<Integer>[]) new ArrayList[V];
+		for (int v = 0; v < V; v++) {
+			adj[v] = new ArrayList<Integer>();
+		}              
+	}
+
+	//Returns current vertex
+	public int V() {
+		return V;	
+	}
+
+	public int E() {
+		return E;
+	}
+
+	//Adds a directed edge from v->w
+	public void addEdge(int v, int w)
+	{
+		if((validateVertex(v)>0)&&(validateVertex(w)>0))
+		{
+			adj[v].add(w);
+			indegree[w]++;
+			E++;
+		}
+		else{
+			System.out.println("Please enter vertices between 0 & n-1");
+		}
+
+	}
+
+	private int validateVertex(int v) {
+		if (v < 0 || v >= V)
+			return -1;
+		else
+			return 1;}
+
+
+	//Returns amount of directed edges incident to vertex v
+	public int indegree(int v) {
+		if(validateVertex(v)<0){
+			return -1;
+		}
+		else{
+			return indegree[v];
 		}
 	}
 
-	@SuppressWarnings("unchecked")
-	private Node[] nodeList = new DAG.Node[0];
-
-
-	public int size() { 
-		return nodeList.length; 
-	}
-
-	// Checks whether Node n is in the graph.
-	public boolean contains(Value v) {
-		boolean valueInList = false;
-		for (int i=0; i<nodeList.length; i++) {
-			if (nodeList[i].val == v) {
-				valueInList = true;
-				break;
-			}			
+	//Returns amount of directed edges from vertex v
+	public int outdegree(int v) {
+		if(validateVertex(v)<0){
+			return -1;
 		}
-		return valueInList;
-	}
-
-
-	public boolean isEmpty() {  //method to check if the DAG is empty
-		if (size()==0) {
-			return true;
-		}
-		else {
-			return false;
+		else{
+			return adj[v].size();
 		}
 	}
 
-	// Converts value to a Node by taking a value and returning associated Node. 
-	public Node retrieveNodefromValue (Value v){
-		Node nodeToRetrieve = new Node(null);
-		if(v == null) {
-			return null;
-		}
-		for (int i=0; i<nodeList.length; i++){
-			if (nodeList[i].val == v){
-				nodeToRetrieve = nodeList[i];
-				break;			
-			}	
-		}
-		return nodeToRetrieve;
-	}
 
-	public void put(Value v, Value fromVal, Value toVal) {	
-		Node n = new Node(v);
-		Node from = retrieveNodefromValue(fromVal);
-		Node to = retrieveNodefromValue(toVal);
+	//Returns the adjacent vertices to v
+	public Iterable<Integer> adj(int v)
+	{ return adj[v]; }
 
-		if (fromVal != null) {
-			from.successors = extendArray(from.successors); 
-			from.successors[from.successors.length-1] = n;
-			if (from.val == null) {
-				addNodeToNodeList(from); 
+	//to find the LCA, will have to traverse the graph backwards as the lca comes before the two nodes
+	public DAG reverse() {
+		DAG reverse = new DAG(V); //new dag of same parameter
+		for (int v = 0; v < V; v++) {
+			for (int w : adj(v)) {
+				reverse.addEdge(w, v); //reverse the direction of the edges
 			}
 		}
-
-		if (toVal != null) {
-			n.successors = extendArray(n.successors);
-			n.successors[n.successors.length-1] = to;
-			if (to.val == null) {
-				addNodeToNodeList(to);
-			}
-		}
-		addNodeToNodeList(n);
+		return reverse;
 	}
 
-	public void deleteNode(Node n) {
-		int i;
-		for(i=0; i<n.successors.length; i++) {
-			n.successors[i] = null;
-		}
 
-		for(i=0; i<nodeList.length; i++) {
-			if (Arrays.asList(nodeList[i].successors).contains(n) == true) {
-				for (int j=0; j<nodeList[i].successors.length; j++) {
-					if (nodeList[i].successors[j] == n) {
-						nodeList[i].successors[j] = null;
-					}
+	public ArrayList<Integer> BFS(int s)
+	{
+		// Mark all the vertices as not visited(By default set as false)
+		boolean visited[] = new boolean[V];
+
+		LinkedList<Integer> queue = new LinkedList<Integer>();
+		ArrayList<Integer> order= new ArrayList<Integer>();
+
+		visited[s]=true;
+		queue.add(s);
+
+
+		while (queue.size() != 0)
+		{
+			// Dequeue a vertex from queue and print it
+			s = queue.poll();           
+			order.add(s);
+			// Get all adjacent vertices of the dequeued vertex s
+			// If a adjacent has not been visited, then mark it
+			// visited and enqueue it
+			Iterator<Integer> i = adj[s].listIterator();
+			while (i.hasNext())
+			{
+				int n = i.next();
+				if (!visited[n])
+				{
+					visited[n] = true;
+					queue.add(n);
 				}
 			}
 		}
-		for (int k=0; k<nodeList.length; k++) {
-			if (nodeList[k] == n) {
-				nodeList[k] = null;
+
+		return order;
+
+	}
+
+	public boolean hasCycle() {
+
+		return hasCycle;
+	}
+
+	public void findCycle(int v) {
+
+		marked[v] = true;
+		stack[v] = true;
+
+		for (int w : adj(v)) {
+			if(!marked[w]) {
+				findCycle(w);
+			} else if (stack[w]) {
+				hasCycle = true;
+				return;
 			}
-		}		
-	}
-
-	public void addNodeToNodeList (Node n) {
-		nodeList = extendArray(nodeList);
-		nodeList[nodeList.length-1] = n;
-	}
-
-	// To extend the array 
-	@SuppressWarnings("unchecked")
-	public Node[] extendArray(Node[] originalArray) {
-		Node[] newArray = new DAG.Node[originalArray.length+1];
-		System.arraycopy(originalArray, 0, newArray, 0, originalArray.length);
-		return newArray;
+		}
+		stack[v] = false;
 	}
 
 }
-
-
-
-
-
